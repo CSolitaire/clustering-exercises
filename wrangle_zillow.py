bb5f25efe7fc60a2c1fb61e4ee940c943bc95ff9
Corey Solitaire <corey.solitaire@gmail.com>
2020-10-12 09:50:53 -0500

    Bugs in Import Fiels

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -51,121 +51,7 @@ def get_zillow_data(cached=False):
 
 #################### Prepare ##################
 
-def get_object_cols(df):
-    '''
-    This function takes in a dataframe and identifies the columns that are object types
-    and returns a list of those column names. 
-    '''
-    # create a mask of columns whether they are object type or not
-    mask = np.array(df.dtypes == "object")
-
-    # get a list of the column names that are objects (from the mask)
-    object_cols = df.iloc[:, mask].columns.tolist()
-    
-    return object_cols
-    
-
-def create_dummies(df, object_cols):
-    '''
-    This function takes in a dataframe and list of object column names,
-    and creates dummy variables of each of those columns. 
-    It then appends the dummy variables to the original dataframe. 
-    It returns the original df with the appended dummy variables. 
-    '''
-    
-    # run pd.get_dummies() to create dummy vars for the object columns. 
-    # we will drop the column representing the first unique value of each variable
-    # we will opt to not create na columns for each variable with missing values 
-    # (all missing values have been removed.)
-    dummy_df = pd.get_dummies(object_cols, dummy_na=False, drop_first=True)
-    
-    # concatenate the dataframe with dummies to our original dataframe
-    # via column (axis=1)
-    df = pd.concat([df, dummy_df], axis=1)
-
-    return df
-
-    
-def train_validate_test(df, target):
-    '''
-    this function takes in a dataframe and splits it into 3 samples, 
-    a test, which is 20% of the entire dataframe, 
-    a validate, which is 24% of the entire dataframe,
-    and a train, which is 56% of the entire dataframe. 
-    It then splits each of the 3 samples into a dataframe with independent variables
-    and a series with the dependent, or target variable. 
-    The function returns 3 dataframes and 3 series:
-    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test. 
-    '''
-    # split df into test (20%) and train_validate (80%)
-    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
-
-    # split train_validate off into train (70% of 80% = 56%) and validate (30% of 80% = 24%)
-    train, validate = train_test_split(train_validate, test_size=.3, random_state=123)
-
-    # split train into X (dataframe, drop target) & y (series, keep target only)
-    #X_train = train.drop(columns=[target]) # No Target in Clustering
-    #y_train = train[target] # No Target in Clustering
-    
-    # split validate into X (dataframe, drop target) & y (series, keep target only)
-    #X_validate = validate.drop(columns=[target]) # No Target in Clustering
-    #y_validate = validate[target] # No Target in Clustering
-    
-    # split test into X (dataframe, drop target) & y (series, keep target only)
-    #X_test = test.drop(columns=[target]) # No Target in Clustering
-    #y_test = test[target]  # No Target in Clustering
-    
-    #return X_train, y_train, X_validate, y_validate, X_test, y_test
-    return train, validate, test
-
-def get_numeric_X_cols(train, object_cols):
-    '''
-    takes in a dataframe and list of object column names
-    and returns a list of all other columns names, the non-objects. 
-    '''
-    numeric_cols = [col for col in train.columns.values if col not in object_cols]
-        
-    return numeric_cols
-
-
-def min_max_scale(train, validate, test, numeric_cols):
-    '''
-    this function takes in 3 dataframes with the same columns, 
-    a list of numeric column names (because the scaler can only work with numeric columns),
-    and fits a min-max scaler to the first dataframe and transforms all
-    3 dataframes using that scaler. 
-    it returns 3 dataframes with the same column names and scaled values. 
-    '''
-    # create the scaler object and fit it to X_train (i.e. identify min and max)
-    # if copy = false, inplace row normalization happens and avoids a copy (if the input is already a numpy array).
-
-    scaler = MinMaxScaler(copy=True).fit(X_train[numeric_cols])
-
-    #scale X_train, X_validate, X_test using the mins and maxes stored in the scaler derived from X_train. 
-    # 
-    train_scaled_array = scaler.transform(train[numeric_cols])
-    validate_scaled_array = scaler.transform(validate[numeric_cols])
-    test_scaled_array = scaler.transform(test[numeric_cols])
-
-    # convert arrays to dataframes
-    train_explore = train
-
-    train_scaled = pd.DataFrame(train_scaled_array, 
-                                  columns=numeric_cols).\
-                                  set_index([train.index.values])
-
-    validate_scaled = pd.DataFrame(validate_scaled_array, 
-                                     columns=numeric_cols).\
-                                     set_index([validate.index.values])
-
-    test_scaled = pd.DataFrame(test_scaled_array, 
-                                 columns=numeric_cols).\
-                                 set_index([test.index.values])
-
-    
-    return train_explore, train_scaled, validate_scaled, test_scaled
-
- # Function to Prep Data (Delete Columns and Rows)
+# Function to Prep Data (Delete Columns and Rows)
 def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=.75):
     
     def remove_columns(df, cols_to_remove):  
@@ -184,87 +70,72 @@ def data_prep(df, cols_to_remove=[], prop_required_column=.5, prop_required_row=
     df.dropna(inplace=True) # Drops all Null Values From Dataframe
     return df
 
-def wrangle_zillow(path):
-    df = pd.read_csv(path)
-    # Rename columns for clarity
-    df.rename(columns={"hashottuborspa":"hottub_spa","fireplacecnt":"fireplace","garagecarcnt":"garage"}, inplace = True) 
-    # Replaces NaN values with 0
-    df['garage'] = df['garage'].replace(np.nan, 0)
-    df['hottub_spa'] = df['hottub_spa'].replace(np.nan, 0)
-    df['lotsizesquarefeet'] = df['lotsizesquarefeet'].replace(np.nan, 0)
-    df['poolcnt'] = df['poolcnt'].replace(np.nan, 0)
-    df['fireplace'] = df['fireplace'].replace(np.nan, 0)
-    ## Add dummy variables as new columns in dataframe and rename them, delete origional
-    df["zip"] = df["regionidzip"].astype('category')
-    df["zip"] = df["zip"].cat.codes
-    ## Add dummy variables as new columns in dataframe and rename them, delete origional
-    df["propertylandusetypeid"]=df["propertylandusetypeid"].astype('category')
-    df["zip"] = df["zip"].cat.codes
+def wrangle_zillow():
+    df = pd.read_csv('zillow_df.csv')
     
-    ''''''
     # Rename duplicate column id/id with id_delete/id
-    df.columns = ['parcelid', 'typeconstructiontypeid', 'storytypeid',
-       'propertylandusetypeid', 'heatingorsystemtypeid', 'buildingclasstypeid',
-       'architecturalstyletypeid', 'airconditioningtypeid', 'id_delete',
-       'basementsqft', 'bathroomcnt', 'bedroomcnt', 'buildingqualitytypeid',
-       'calculatedbathnbr', 'decktypeid', 'finishedfloor1squarefeet',
-       'calculatedfinishedsquarefeet', 'finishedsquarefeet12',
-       'finishedsquarefeet13', 'finishedsquarefeet15', 'finishedsquarefeet50',
-       'finishedsquarefeet6', 'fips', 'fireplacecnt', 'fullbathcnt',
-       'garagecarcnt', 'garagetotalsqft', 'hashottuborspa', 'latitude',
-       'longitude', 'lotsizesquarefeet', 'poolcnt', 'poolsizesum',
-       'pooltypeid10', 'pooltypeid2', 'pooltypeid7',
-       'propertycountylandusecode', 'propertyzoningdesc',
-       'rawcensustractandblock', 'regionidcity', 'regionidcounty',
-       'regionidneighborhood', 'regionidzip', 'roomcnt', 'threequarterbathnbr',
-       'unitcnt', 'yardbuildingsqft17', 'yardbuildingsqft26', 'yearbuilt',
-       'numberofstories', 'fireplaceflag', 'structuretaxvaluedollarcnt',
-       'taxvaluedollarcnt', 'assessmentyear', 'landtaxvaluedollarcnt',
-       'taxamount', 'taxdelinquencyflag', 'taxdelinquencyyear',
-       'censustractandblock', 'id', 'logerror', 'pid', 'tdate',
-       'airconditioningdesc', 'architecturalstyledesc', 'buildingclassdesc',
-       'heatingorsystemdesc', 'propertylandusedesc', 'storydesc',
-       'typeconstructiondesc']
-    # Delete duplicate columns id_delete and pid (dupilcate of parcelid)
-    df.drop(columns = ['id_delete','pid'], inplace = True)
-    # Sorting for single unit properties
-    unit_df = df[df['unitcnt']==1]
-    room_df = df[df['roomcnt']>0]
-    garage_df = df[df['garagecarcnt']>0]
-    bed_df = df[df['bedroomcnt']>0]
-    bath_df = df[df['bathroomcnt']>0]
-    seconday_featutes_df = pd.concat([bed_df, bath_df, garage_df, room_df, unit_df]).drop_duplicates('id').reset_index(drop=True)
-    p261_df = df[df['propertylandusetypeid'] == 261]
-    p263_df = df[df['propertylandusetypeid'] == 263]
-    p264_df = df[df['propertylandusetypeid'] == 264]    
-    p266_df = df[df['propertylandusetypeid'] == 266] 
-    p273_df = df[df['propertylandusetypeid'] == 273]
-    p275_df = df[df['propertylandusetypeid'] == 275]
-    p276_df = df[df['propertylandusetypeid'] == 276]    
-    p279_df = df[df['propertylandusetypeid'] == 279]
-    property_df = pd.concat([p261_df, p263_df, p264_df, p266_df, p273_df, p275_df, p276_df, p279_df]).drop_duplicates('id').reset_index(drop=True)
-    df = pd.concat([seconday_featutes_df, property_df]).drop_duplicates('id').reset_index(drop=True)
-    ''''''
+    def rename_columns(df):
+        df.columns = ['parcelid', 'typeconstructiontypeid', 'storytypeid',
+            'propertylandusetypeid', 'heatingorsystemtypeid', 'buildingclasstypeid',
+            'architecturalstyletypeid', 'airconditioningtypeid', 'id_delete',
+            'basementsqft', 'bathroomcnt', 'bedroomcnt', 'buildingqualitytypeid',
+            'calculatedbathnbr', 'decktypeid', 'finishedfloor1squarefeet',
+            'calculatedfinishedsquarefeet', 'finishedsquarefeet12',
+            'finishedsquarefeet13', 'finishedsquarefeet15', 'finishedsquarefeet50',
+            'finishedsquarefeet6', 'fips', 'fireplacecnt', 'fullbathcnt',
+            'garagecarcnt', 'garagetotalsqft', 'hashottuborspa', 'latitude',
+            'longitude', 'lotsizesquarefeet', 'poolcnt', 'poolsizesum',
+            'pooltypeid10', 'pooltypeid2', 'pooltypeid7',
+            'propertycountylandusecode', 'propertyzoningdesc',
+            'rawcensustractandblock', 'regionidcity', 'regionidcounty',
+            'regionidneighborhood', 'regionidzip', 'roomcnt', 'threequarterbathnbr',
+            'unitcnt', 'yardbuildingsqft17', 'yardbuildingsqft26', 'yearbuilt',
+            'numberofstories', 'fireplaceflag', 'structuretaxvaluedollarcnt',
+            'taxvaluedollarcnt', 'assessmentyear', 'landtaxvaluedollarcnt',
+            'taxamount', 'taxdelinquencyflag', 'taxdelinquencyyear',
+            'censustractandblock', 'id', 'logerror', 'pid', 'tdate',
+            'airconditioningdesc', 'architecturalstyledesc', 'buildingclassdesc',
+            'heatingorsystemdesc', 'propertylandusedesc', 'storydesc',
+            'typeconstructiondesc']
+        df.drop(columns = ['id_delete','pid'], inplace = True)
+    return df
 
-    # Function to Prep Data (Delete Columns and Rows)
+    # Rename columns with duplicate id's
+    df = rename_columns(df)
+
+    # Prep Data Function
     df = data_prep(df)
 
-    # get object column names
-    object_cols = get_object_cols(df)
-        
-    # create dummy vars
-    df = create_dummies(df, object_cols)
-        
-    # split data (taxvaluedollarcnt is target)
-    X_train, y_train, X_validate, y_validate, X_test, y_test = train_validate_test(df, 'taxvaluedollarcnt')
-        
-    # get numeric column names
-    numeric_cols = get_numeric_X_cols(X_train, object_cols)
+    # split dataset
+    train_validate, test = train_test_split(df, test_size = .2, random_state = 123)
+    train, validate = train_test_split(train_validate, test_size = .3, random_state = 123)
 
-    # scale data 
-    X_train_scaled, X_validate_scaled, X_test_scaled = min_max_scale(X_train, X_validate, X_test, numeric_cols)
+    # Assign variables
+    X_train = train
+    X_validate = validate
+    X_test = test
+    X_train_explore = train
 
-    return df, train_explore, train_scaled, validate_scaled, test_scaled
+    # Scale data
+    scaler = MinMaxScaler(copy=True).fit(X_train)
+
+    X_train_scaled = scaler.transform(X_train)
+    X_validate_scaled = scaler.transform(X_validate)
+    X_test_scaled = scaler.transform(X_test)
+
+    X_train_scaled = pd.DataFrame(X_train_scaled, 
+                                columns=X_train.columns.values).\
+                                set_index([X_train.index.values])
+
+    X_validate_scaled = pd.DataFrame(X_validate_scaled, 
+                                    columns=X_validate.columns.values).\
+                                set_index([X_validate.index.values])
+
+    X_test_scaled = pd.DataFrame(X_test_scaled, 
+                                    columns=X_test.columns.values).\
+                                set_index([X_test.index.values])
+
+    return df, X_train_explore, X_train_scaled, X_validate_scaled, X_test_scaled
 
     ################## Explore ####################
 
